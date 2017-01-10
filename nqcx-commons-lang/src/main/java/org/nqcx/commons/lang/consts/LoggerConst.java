42182124dbcd88db9be015fdbb4bb969fb7215df
Naqi Chuan <naqichuan@163.com>
2017-01-10 09:05:39 +0800

    增加常量定义

---
+++ nqcx-commons-lang/src/main/java/org/nqcx/commons/lang/consts/LoggerConst.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 nqcx.org All right reserved. This software is the confidential and proprietary information
+ * of nqcx.org ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with nqcx.org.
+ */
+
+package org.nqcx.commons.lang.consts;
+
+/**
+ * 日志常量
+ *
+ * @author naqichuan 17/1/10 08:43
+ */
+public class LoggerConst {
+
+    /**
+     * 缓存日志名
+     */
+    public final static String LOGGER_CACHE_NAME = "_NQCX_LOGGER_CACHE";
+
+    /**
+     * 消息队列日志名
+     */
+    public final static String LOGGER_MQ_NAME = "_NQCX_LOGGER_MQ";
+
+    /**
+     * 访问日志名
+     */
+    public final static String LOGGER_ACCESS_NAME = "_NQCX_LOGGER_ACCESS";
+}
---
+++ nqcx-commons-lang/src/main/java/org/nqcx/commons/lang/consts/PeriodConst.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2017 nqcx.org All right reserved. This software is the confidential and proprietary information
+ * of nqcx.org ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with nqcx.org.
+ */
+
+package org.nqcx.commons.lang.consts;
+
+/**
+ * 时间段常量
+ *
+ * @author naqichuan 17/1/10 08:45
+ */
+public class PeriodConst {
+
+    public final static int ONE_MINUTES = 60; // 以秒为单位
+    public final static int TEN_MINUTES = 10 * ONE_MINUTES;
+    public final static int THIRTY_MINUTES = 30 * ONE_MINUTES;
+    public final static int ONE_HOUR = 60 * ONE_MINUTES;
+    public final static int ONE_DAY = 24 * ONE_HOUR;
+}
