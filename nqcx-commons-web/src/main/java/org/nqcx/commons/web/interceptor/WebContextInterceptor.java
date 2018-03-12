90c002d9a51e70b5485458de2595362af0a452f1
Naqi Chuan <naqichuan@163.com>
2018-03-12 13:39:09 +0800

    host address

--- nqcx-commons-web/src/main/java/org/nqcx/commons/web/interceptor/WebContextInterceptor.java
+++ nqcx-commons-web/src/main/java/org/nqcx/commons/web/interceptor/WebContextInterceptor.java
@@ -89,7 +89,6 @@ public class WebContextInterceptor extends WebSupport implements HandlerIntercep
                 webContext.getRemoteAddr(), webContext.getMethod(), webContext.getScheme(), webContext.isSecure(), webContext.isAjax(),
                 webContext.getRequestURI(), webContext.getLocale(), trimToEmpty(webContext.getSessionId()), webContext.getUrl(),
                 trimToEmpty(webContext.getReferer()), trimToEmpty(webContext.getUserAgent()));
-
         return true;
     }
 
