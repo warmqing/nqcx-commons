2279e525eac0ce3f8cd8204b125b4de5e4fdc324
Naqi Chuan <naqichuan@163.com>
2018-03-12 10:19:37 +0800

    host address

--- nqcx-commons-util/src/main/java/org/nqcx/commons/util/server/ServerUtil.java
+++ nqcx-commons-util/src/main/java/org/nqcx/commons/util/server/ServerUtil.java
@@ -1,6 +1,6 @@
-/* 
- * Copyright 2014 nqcx.org All right reserved. This software is the 
- * confidential and proprietary information of nqcx.org ("Confidential 
+/*
+ * Copyright 2014 nqcx.org All right reserved. This software is the
+ * confidential and proprietary information of nqcx.org ("Confidential
  * Information"). You shall not disclose such Confidential Information and shall
  * use it only in accordance with the terms of the license agreement you entered
  * into with nqcx.org.
